164ace7e05bf47b9bfa32f08e95b292c50bc7acf
Mauricio Degregori <degregori2@gmail.com>
2019-12-11 10:19:43 -0600

    server for db up and running

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const UserRouter = require('./users/user-router.js');
 
 const server = express();
 
+server.get('/', (req, res) => {
+    res.send(`<h2>Sanity Test!</h2>`);
+  });
+
+
 server.use(helmet());
 server.use(express.json());
 
